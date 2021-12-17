b4bd08f739efc60f996872bf8a232627d789fb1d
Josh Cough <joshcough@gmail.com>
2021-12-17 08:44:38 -0500

    small reorg of locations.lua

--- locations.lua
+++ locations.lua
@@ -1,7 +1,10 @@
 require "Class"
 enum = require("enum")
 
+-- TODO: import these. but in order to do that, we will have go create a Goal module.
 OverWorldId = 1
+Tantegel    = 4
+Garinham    = 9
 
 Point = class(function(a, mapId, x, y)
   a.mapId = mapId
@@ -123,14 +126,6 @@ function Neighbor:getPoint()
   return Point(self.mapId, self.x, self.y)
 end
 
--- TODO:
--- we need to have important locations for tantegel basement and garinham basement.
--- we need one for the princess too, probably
--- we might need one for the old man in cantlin, because its behind a locked door
--- so in that case i think we also need one for the shop in cantlin behind a locked door
--- and the chests inside rimuldar and charlock
--- but i think chests will just sorta get added automatically. or they should...
-
 GoalType = enum.new("GoalType", {
   "CHARLOCK",
   "TANTEGEL",
@@ -138,31 +133,9 @@ GoalType = enum.new("GoalType", {
   "CAVE",
   "CHEST",
   "SPIKE",
-  "COORDINATES",
+  "SEARCH",
   "BASEMENT"
 })
-
--- TODO: Here we have a goal that we can't just walk to. similar to how we can't just walk to Charlock
--- in this particular case, we need to walk to the static map and then probably interpret that script
--- or maybe the goal itself has a script! one of the best ideas ever really.
-
--- Goals we can't just walk to:
--- * staff of rain blocked by old man
--- * rainbow drop blocked by jerk
--- * charlock blocked by rainbow drop
-
--- additionally: this Goal has prerequisites!
--- here we are in the Northern Shrine (mapId:13) where we must trade the silver harp for the staff of rain
--- so the Silver Harp goal must be a prerequisite for the Staff of Rain goal
-
--- goal	<ObjectWithPath v: <Goal location:<Point mapId:13, x:3, y:4>,
--- type:<GoalType.CHEST: 5>, seenByPlayer:true, completed:false>,
--- path:<Path  src: <Point mapId:1, x:58, y:106>, dest:<Point mapId:13, x:3, y:4>, weight:17,
--- path: {<Point mapId:1, x:58, y:106>,
--- <Neighbor mapId:1, x:59, y:106, dir: RIGHT>,
---- ...
--- <Neighbor mapId:13, x:4, y:4, dir: LEFT>,
--- <Neighbor mapId:13, x:3, y:4, dir: LEFT>}>>
 
 -- @location :: Point
 -- @type :: GoalType
@@ -199,15 +172,10 @@ Goal_Chest = class(Goal, function(a, chest)
   a.chest = chest
 end)
 
--- TODO: these three seem like they don't really add anything...could possibly just use the Goal constructor
--- swamp cave spike, hauksness spike, charlock spike
-Goal_Spike = class(Goal, function(a, mapId, x, y)
-  Goal.init(a, Point(mapId, x, y), GoalType.SPIKE, Table3D())
-end)
-
 -- the search spot / coordinates / token location or whatever tf.
-Goal_Coordinates = class(Goal, function(a, loc)
-  Goal.init(a, loc, GoalType.COORDINATES, Table3D())
+Goal_SearchSpot = class(Goal, function(a, searchSpot)
+  Goal.init(a, searchSpot.location, GoalType.SEARCH, Table3D())
+  a.searchSpot = searchSpot
 end)
 
 Goal_Basement = class(Goal, function(a, entrance, children)
@@ -215,71 +183,11 @@ Goal_Basement = class(Goal, function(a, entrance, children)
   a.entrance = entrance
 end)
 
---[[
-  * 7 chests in tantegel plus the basement stairs. (and possibly the key shop...)
-  * each town should have one (and possibly have one for each of their shops and inns... maybe?)
-  * garinham has 3 chests and basement stairs.
-  * rimuldar has a chest
-  *& hauksness has a spike tile
-  * canlin has a shop behind a locked door, and an old man too
-  * each of the caves should have an entry
-  * and each of the chests in each of the caves should too
-  * we could potentially add the coordinates here, and just have a
-  * flag that they are inaccessible until we talk to the old man
-  things are accessible if we have been to the parent map (and have keys, if they are behind a door)
-  * swamp cave: spike tile and princess?
-    * also if they go in swamp north... we should add swamp south as an important location, and vice versa.
-      in my opinion anyway, its worth trying. it opens up a huge section of the world, so we might want to make it
-      a priority.
-]]
-
-
--- All goals with prerequisites:
--- * staff of rain requires silver harp
--- * jerk cave requires all three key items (staff, stones, token) to get rainbow drop
--- * charlock requires rainbow drop
--- * coordinates requires either talking to old man or Gwaelin's Love
--- * Gwaelin's Love requires the princess
--- * we could also make it so that each chest has a prerequisite of finding the map that its in,
---   but im not sure that makes sense - we dont have Goal_Town, Goal_Dungeon.
---   instead, we can keep using the seenByPlayer stuff probably.
-
--- NorthernShrine     = 13
--- SouthernShrine     = 14  --- TODO: there IS NO GOAL for SouthernShrine! that's because the chest isn't real.
---                                    so we probably need to specially add one.
-
--- TODO: question: does it even matter what type of goal it is, if every goal has a script? probably not. maybe delete.
-
-
---[[
-TODO:
-what if we start with the overworld, and unfold...
-and instead of adding prerequisites, we add children
-the overworld has many children, and those are the root nodes of the tree
-(since each node must have a location, and the overworld itself does not have a single location)
-for each of those children, they might have chests which will be their children
-and they will have sublevels (potentially), which would lead to more children.
-not sure the sublevels are really needed, we can just immediately make available all
-the chests in the dungeon/town when we enter the top level of it.
-
-right away we automatically find tantegel, and its completed
-and the throneroom, and it is completed.
-and we find all the chests for both. they are seen, but not completed until we open them
-and we find the basement. it is seen, but not completed.
-when we go into the basement, it becomes completed, and any children it has would become available.
-
-we could maintain the tree of prereqs, but also a list of available prereqs.
-as we discover them, they go into the list.
-]]
-
--- TODO: import these. but in order to do that, we will have go create a Goal module.
-Tantegel           = 4
-Garinham           = 9
-
--- ok so how do we write this function?
--- we have to get the basements, if they have any
--- we have to get all the chests, again, if they have any
--- and we can add any other map specific things here too with a simple if statement.
+-- TODO: is this actually needed?
+-- -- swamp cave spike, hauksness spike, charlock spike
+-- Goal_Spike = class(Goal, function(a, mapId, x, y)
+--   Goal.init(a, Point(mapId, x, y), GoalType.SPIKE, Table3D())
+-- end)
 
 -- TODO: maybe we should make a StaticMaps, and then maybe even a Maps (which includes the overworld)
 -- @staticMaps :: [StaticMap]
@@ -293,11 +201,9 @@ function buildAllGoals(allStaticMaps, allChests, searchSpots)
 
   -- @staticMap :: StaticMap
   function childGoalsOfStaticMap(staticMap)
-    log.debug("staticMap", staticMap.mapName)
     local res = Table3D()
 
     local mapId = staticMap.mapId
-    log.debug("mapId", mapId)
 
     -- @returns :: ()
     function addBasementGoal()
@@ -334,25 +240,136 @@ function buildAllGoals(allStaticMaps, allChests, searchSpots)
     return res
   end
 
-  log.debug("SEARCH SPOTS", searchSpots)
-
-  local res = Table3D()
-  -- for all the static maps that have entrances, we do stuff
-  for mapId, staticMap in pairs(allStaticMaps) do
-    if staticMap.entrances ~= nil then
-      for _, entrance in pairs(staticMap.entrances) do
-        if entrance.from.mapId == OverWorldId then
-          local goalChildren = childGoalsOfStaticMap(staticMap)
-          local goal = Goal_Overworld(staticMap, entrance, goalChildren)
-          log.debug("adding goal for overworld: ", goal, entrance)
-          res:insert(entrance.from, goal)
+  function addGoalsForAllMaps(res)
+    -- for all the static maps that have entrances, we do stuff
+    for mapId, staticMap in pairs(allStaticMaps) do
+      if staticMap.entrances ~= nil then
+        for _, entrance in pairs(staticMap.entrances) do
+          if entrance.from.mapId == OverWorldId then
+            local goalChildren = childGoalsOfStaticMap(staticMap)
+            local goal = Goal_Overworld(staticMap, entrance, goalChildren)
+            log.debug("adding goal for overworld: ", goal, entrance)
+            res:insert(entrance.from, goal)
+          end
         end
       end
     end
   end
 
+  function addGoalsForAllSearchSpots(res)
+    function addSearchSpotGoal(searchSpot)
+      local goal = Goal_SearchSpot(searchSpot)
+      if searchSpot.item == SilverHarp then harpGoal = goal end
+      log.debug("adding goal for search spot: ", goal, entrance)
+      res:insert(searchSpot.location, goal)
+    end
+    addSearchSpotGoal(searchSpots.coordinates)
+    addSearchSpotGoal(searchSpots.kol)
+    addSearchSpotGoal(searchSpots.hauksness)
+  end
+
+  local res = Table3D()
+  addGoalsForAllMaps(res)
+  addGoalsForAllSearchSpots(res)
   harpGoal.children:insert(northernShrineGoal.location, northernShrineGoal)
 
   res:debug("ALL GOALS")
   return res
 end
+
+-- TODO:
+-- we need to have important locations for tantegel basement and garinham basement.
+-- we need one for the princess too, probably
+-- we might need one for the old man in cantlin, because its behind a locked door
+-- so in that case i think we also need one for the shop in cantlin behind a locked door
+-- and the chests inside rimuldar and charlock
+-- but i think chests will just sorta get added automatically. or they should...
+
+-- TODO: Here we have a goal that we can't just walk to. similar to how we can't just walk to Charlock
+-- in this particular case, we need to walk to the static map and then probably interpret that script
+-- or maybe the goal itself has a script! one of the best ideas ever really.
+
+-- Goals we can't just walk to:
+-- * staff of rain blocked by old man
+-- * rainbow drop blocked by jerk
+-- * charlock blocked by rainbow drop
+
+-- additionally: this Goal has prerequisites!
+-- here we are in the Northern Shrine (mapId:13) where we must trade the silver harp for the staff of rain
+-- so the Silver Harp goal must be a prerequisite for the Staff of Rain goal
+
+-- goal	<ObjectWithPath v: <Goal location:<Point mapId:13, x:3, y:4>,
+-- type:<GoalType.CHEST: 5>, seenByPlayer:true, completed:false>,
+-- path:<Path  src: <Point mapId:1, x:58, y:106>, dest:<Point mapId:13, x:3, y:4>, weight:17,
+-- path: {<Point mapId:1, x:58, y:106>,
+-- <Neighbor mapId:1, x:59, y:106, dir: RIGHT>,
+--- ...
+-- <Neighbor mapId:13, x:4, y:4, dir: LEFT>,
+-- <Neighbor mapId:13, x:3, y:4, dir: LEFT>}>>
+
+--[[
+  * 7 chests in tantegel plus the basement stairs. (and possibly the key shop...)
+  * each town should have one (and possibly have one for each of their shops and inns... maybe?)
+  * garinham has 3 chests and basement stairs.
+  * rimuldar has a chest
+  *& hauksness has a spike tile
+  * canlin has a shop behind a locked door, and an old man too
+  * each of the caves should have an entry
+  * and each of the chests in each of the caves should too
+  * we could potentially add the coordinates here, and just have a
+  * flag that they are inaccessible until we talk to the old man
+  things are accessible if we have been to the parent map (and have keys, if they are behind a door)
+  * swamp cave: spike tile and princess?
+    * also if they go in swamp north... we should add swamp south as an important location, and vice versa.
+      in my opinion anyway, its worth trying. it opens up a huge section of the world, so we might want to make it
+      a priority.
+]]
+
+-- All goals with prerequisites:
+-- * staff of rain requires silver harp
+-- * jerk cave requires all three key items (staff, stones, token) to get rainbow drop
+-- * charlock requires rainbow drop
+-- * coordinates requires either talking to old man or Gwaelin's Love
+-- * Gwaelin's Love requires the princess
+-- * we could also make it so that each chest has a prerequisite of finding the map that its in,
+--   but im not sure that makes sense - we dont have Goal_Town, Goal_Dungeon.
+--   instead, we can keep using the seenByPlayer stuff probably.
+
+-- NorthernShrine     = 13
+-- SouthernShrine     = 14  --- TODO: there IS NO GOAL for SouthernShrine! that's because the chest isn't real.
+--                                    so we probably need to specially add one.
+
+-- TODO: question: does it even matter what type of goal it is, if every goal has a script? probably not. maybe delete.
+
+--[[
+TODO:
+what if we start with the overworld, and unfold...
+and instead of adding prerequisites, we add children
+the overworld has many children, and those are the root nodes of the tree
+(since each node must have a location, and the overworld itself does not have a single location)
+for each of those children, they might have chests which will be their children
+and they will have sublevels (potentially), which would lead to more children.
+not sure the sublevels are really needed, we can just immediately make available all
+the chests in the dungeon/town when we enter the top level of it.
+
+right away we automatically find tantegel, and its completed
+and the throneroom, and it is completed.
+and we find all the chests for both. they are seen, but not completed until we open them
+and we find the basement. it is seen, but not completed.
+when we go into the basement, it becomes completed, and any children it has would become available.
+
+we could maintain the tree of prereqs, but also a list of available prereqs.
+as we discover them, they go into the list.
+]]
+
+-- TODO: search spots, scripts for everything?
+-- TODO: the southern shrine doesn't fit the `children` model, but instead fits the prereq model. :(
+-- it has a prereq of (Stones AND Token AND Staff)
+-- similarly, the coordinates too. it has a prereq of (OldMan OR Princess)
+
+-- TODO:
+-- i think we need a goal for the old man in cantlin
+-- and we need to have the coordinates be a child of that
+-- but the coordinates can _also_ be a child of the princess...
+-- a princess goal doesnt exist. it will be a little more difficult than the others
+-- since we complete it when we return her. but, can be done.
